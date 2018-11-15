36d9e48078f290567061f0d9e0f8c2763f2349b4
Meigyoku Thmn <bestfriends_0705@yahoo.com.vn>
2018-11-15 10:28:05 +0700

    add minor changes: add more description to README.md add indicator

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ npm i
 node index
 ```
 Then go to http://localhost:3000
+
+### Explaination
+This page do the following: <br />
+
+1. Randomly scatter 100 dots in the graph
+2. Draw a line with random coefficients
+3. Use gradient descent algorithm to modify line's coefficients until Total distance of all dots to the line (on yAxis) is minimum. <br />
+
+(The corresponding tensorflowjs api is tf.train.sgd, stands for Stochastic gradient descent, but it's just a normal gradient descent optimizer despite it's name)
--- client/app.js
+++ client/app.js
@@ -1,37 +1,79 @@
 function getRandomInclusive(min, max) {
    return Math.random() * (max - min + 1) + min;
 }
+var MAX = 1.5;
+var MIN = -1.5;
+var learningRateElement = document.getElementById('learningRate')
+var iterElement = document.getElementById('iter')
+var totalIterElement = document.getElementById('totalIter')
+var lossElement = document.getElementById('loss')
+var co_a_Element = document.getElementById('co_a')
+var co_b_Element = document.getElementById('co_b')
+var ctx = document.getElementById("myChart");
 function makeScatterData() {
    var rs = [];
-   var a = Math.random();
+   var a = getRandomInclusive(MIN, MAX)
    for (var i = 0; i < 100; i++) {
       var x = getRandomInclusive(0, 10);
-      var b = getRandomInclusive(0, 1);
+      var b = getRandomInclusive(-0.2, 0.2) + 5 - 5 * a;
       rs.push({
          x: x,
-         y: a * x + b
+         y: a * x + b,
       });
    }
    return rs;
 }
 function makeLineData(coeffArray) {
    return [{
-      x: 0, y: coeffArray[0] * 0 + coeffArray[1]
+      x: -1, y: coeffArray[0] * (-1) + coeffArray[1]
    }, {
-      x: 10, y: coeffArray[0] * 10 + coeffArray[1]
+      x: 11, y: coeffArray[0] * 11 + coeffArray[1]
    }];
 }
+function halt() {
+   unlockInput();
+   isTraining = false;
+}
+window.iterationNumber = 2000;
+window.learningRate = 0.02;
 function reset() {
-   window.a = tf.variable(tf.scalar(Math.random()))
-   window.b = tf.variable(tf.scalar(Math.random()))
+   unlockInput();
+   window.a = tf.scalar(getRandomInclusive(MIN, MAX)).variable();
+   window.b = tf.scalar(getRandomInclusive(-0.5, 0.5)).add(5).sub(a.mul(5)).variable();
    window.lineData = makeLineData([a.dataSync()[0], b.dataSync()[0]]);
    window.scatterData = [];
    window.isTraining = false;
+   window.iterationNumber = iterationNumber;
+   window.learningRate = learningRate;
+   window.currentIterationNumber = 0;
+   window.loss = '----';
    scatterData = makeScatterData()
    drawChart();
 }
+function onIterationNumberChange(newValue) {
+   iterationNumber = newValue;
+}
+function onLearningRateChange(newValue) {
+   learningRate = newValue;
+}
+function unlockInput() {
+   learningRateElement.readOnly = false;
+   totalIterElement.readOnly = false;
+}
+function lockInput() {
+   learningRateElement.readOnly = true;
+   totalIterElement.readOnly = true;
+}
+function drawIndicator() {
+   learningRateElement.value = learningRate;
+   totalIterElement.value = iterationNumber;
+   iterElement.innerHTML = currentIterationNumber.toString();
+   lossElement.innerHTML = loss.toString();
+   co_a_Element.innerHTML = a.dataSync()[0].toString();
+   co_b_Element.innerHTML = b.dataSync()[0].toString();
+}
 function drawChart(dontUpdateScatterData) {
-   var ctx = document.getElementById("myChart");
+   drawIndicator();
    if (ctx.myChart == null) {
       ctx.myChart = new Chart.Scatter(ctx, {
          data: {
@@ -80,8 +122,7 @@ function drawChart(dontUpdateScatterData) {
 function train() {
    if (isTraining) return;
    isTraining = true;
-   const iterationNumber = 2000;
-   const learningRate = 0.001;
+   lockInput();
    const optimizer = tf.train.sgd(learningRate)
    function predict(x) {
       return tf.tidy(() => {
@@ -97,13 +138,10 @@ function train() {
          isTraining = false;
          return;
       }
+      currentIterationNumber = i + 1;
       optimizer.minimize(function () {
          var stepLoss = calculateLoss(predict(x), y);
-         console.log({
-            loss: stepLoss.dataSync()[0],
-            a: a.dataSync()[0],
-            b: b.dataSync()[0],
-         })
+         loss = stepLoss.dataSync()[0]
          return stepLoss;
       });
       lineData = makeLineData([a.dataSync()[0], b.dataSync()[0]]);
